7fc003e4f2bbc760e4881a629a23c549b5b6537d
tomykaira <tomykaira@gmail.com>
2012-04-17 23:12:13 +0900

    Git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmacsKentei::Application.config.secret_token = '133d3b73d23bb5367ad6446627ac961d4887cbf76be088d0c10ab30636ecf7dee63358fa24acd6c22ecb7aff9e823e54e431b37f2acd1c690cd02ec94adc8120'
