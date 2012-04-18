b5f2311afbbd8e6fdf1027b4316e20c3fd0793b8
tomykaira <tomykaira@gmail.com>
2012-04-18 10:54:03 +0900

    Fix user's foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates_presence_of :uid, :name, :key, :secret
   validates_uniqueness_of :uid
 
-  has_many :problems
+  has_many :problems, foreign_key: :creator_id
 
   def self.create_with_omniauth(auth)
     create!(uid: auth["uid"], name: auth["info"]["nickname"], key: auth.credentials.token, secret: auth.credentials.secret)
